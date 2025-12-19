3d1adec6259e9a46de025af7fd14e9b0a88be22a
Rodion-AI <lidofgen@gmail.com>
2025-12-19 21:16:24 +0700

    add price list; add env

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from p-qri!")
+from langchain.agents import create_agent
 
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
 
-if __name__ == "__main__":
-    main()
+agent = create_agent(
+    model="claude-sonnet-4-5-20250929",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+# Run the agent
+agent.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in sf"}]}
+)
